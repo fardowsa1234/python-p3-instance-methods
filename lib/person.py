6a11060271adbed142b0ceed3c6679e880782b8a
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-05-26 20:36:09 +0300

    python

--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,9 @@ class Person:
 
     #Instance method definition
     pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
